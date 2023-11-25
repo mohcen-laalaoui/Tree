88d6fc5662f396a01b7ecba7e9753f6f308dae3f
Mohcen Laalaoui <134273543+mohcen-laalaoui@users.noreply.github.com>
2023-11-25 19:51:08 +0100

    first

--- listDoublement.cpp
+++ listDoublement.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+// Define a doubly linked list node structure
 struct list
 {
     int data;
@@ -8,18 +9,20 @@ struct list
     list *prev;
 };
 
+// Declare global pointers for the head, tail, and temporary nodes
 list *head;
 list *fin;
 list *p;
 list *q;
 int i, n, number, value;
 
+// Function to display the elements of the list from the first to the last
 void displayFirstLast(list *&head)
 {
     p = head;
     if (p != nullptr)
     {
-        cout << " The elements of the list are: " << endl;
+        cout << "The elements of the list from first to last are: " << endl;
         while (p != nullptr)
         {
             cout << p->data << endl;
@@ -28,13 +31,14 @@ void displayFirstLast(list *&head)
     }
 }
 
+// Function to display the elements of the list from the last to the first
 void displayLastFirst(list *&fin)
 {
     p = fin;
 
     if (p != nullptr)
     {
-        cout << " The elements of the list are: " << endl;
+        cout << "The elements of the list from last to first are: " << endl;
         while (p != nullptr)
         {
             cout << p->data << endl;
@@ -45,17 +49,25 @@ void displayLastFirst(list *&fin)
 
 int main()
 {
-    cout << "Give a number: ";
+    // User input for the number of items in the doubly linked list
+    cout << "Enter the number of items: ";
     cin >> number;
-    cout << "Creation of the doubly linked list which contains " << number << " items: " << endl;
+
+    // Initialization of pointers
     p = head;
+
+    // Loop to create the doubly linked list
     for (i = 1; i <= number; i++)
     {
-        cout << "Give a number: " << endl;
+        cout << "Enter a number: ";
         cin >> value;
+
+        // Create a new node
         q = new list();
         q->data = value;
         q->next = nullptr;
+
+        // Update pointers to maintain the doubly linked list
         if (head != nullptr)
         {
             p->next = q;
@@ -63,16 +75,24 @@ int main()
         }
         else
         {
+            // If the list is empty, set head and fin to the new node
             head = q;
             head->prev = nullptr;
             fin = q;
         }
+
+        // Move the pointer to the new node
         p = q;
         fin = q;
     }
+
+    // Display the doubly linked list from first to last
     cout << "Display from first to last item: " << endl;
     displayFirstLast(head);
+
+    // Display the doubly linked list from last to first
     cout << "Display from last to first item: " << endl;
     displayLastFirst(head);
+
     return 0;
 }
